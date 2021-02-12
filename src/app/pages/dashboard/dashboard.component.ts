5366bda2ee3be4f2f64f7e76d5ff117f9e569778
drzioner <drzioner@gmail.com>
2021-02-12 00:10:29 -0500

    UPDATE: actualizacion en el diseño de la tabla ultimas transacciones

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,18 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { combineLatest } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { Credit } from 'src/app/models/credit.model';
-import { Transaction } from 'src/app/models/transaction.model';
-import { AccountsService } from 'src/app/services/accounts.service';
-import { CreditsService } from 'src/app/services/credits.service';
-import { TransactionsService } from 'src/app/services/transactions.service';
+import {Component, OnInit} from '@angular/core';
+import {combineLatest} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {Credit} from 'src/app/models/credit.model';
+import {Transaction} from 'src/app/models/transaction.model';
+import {AccountsService} from 'src/app/services/accounts.service';
+import {CreditsService} from 'src/app/services/credits.service';
+import {TransactionsService} from 'src/app/services/transactions.service';
 import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styles: [
-  ]
+  styleUrls: ['dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
 
@@ -20,7 +19,7 @@ export class DashboardComponent implements OnInit {
   public credits: Credit[] = [];
 
   public fulldata = combineLatest([
-    this.accountService.getAccounts(0,0, true),
+    this.accountService.getAccounts(0, 0, true),
     this.transactionService.getTransactions(1, 5),
     this.CreditsService.getCredits(1, 5),
   ]).pipe(
@@ -29,32 +28,53 @@ export class DashboardComponent implements OnInit {
         accounts: data[0],
         transactions: data[1],
         credits: data[2]
-      }
+      };
     })
   );
 
   constructor(private accountService: AccountsService,
-    private transactionService: TransactionsService,
-    private CreditsService: CreditsService) { }
+              private transactionService: TransactionsService,
+              // tslint:disable-next-line:no-shadowed-variable
+              private CreditsService: CreditsService) {
+  }
 
   ngOnInit(): void {
     this.fulldata
-    .subscribe(resp => {
-      console.log("combinados",resp);
-      this.transactions = resp.transactions.transactions;
-      this.credits = resp.credits.credits;
-    }, err => {
-      Swal.fire('Error', "Ha ocurrido un error inesperado", 'error');
-    });
+      .subscribe(resp => {
+        console.log('combinados', resp);
+        this.transactions = resp.transactions.transactions;
+        this.credits = resp.credits.credits;
+      }, err => {
+        Swal.fire('Error', 'Ha ocurrido un error inesperado', 'error');
+      });
   }
 
   getAccounts(): void {
     this.accountService.getAccounts(0, 0, true)
-    .subscribe(resp => {
-      console.log(resp);
-    }, err => {
-      Swal.fire('Error', "Ha ocurrido un error inesperado", 'error');
-    });
+      .subscribe(resp => {
+        console.log(resp);
+      }, err => {
+        Swal.fire('Error', 'Ha ocurrido un error inesperado', 'error');
+      });
+  }
+
+  getClassBadge(item: string): string {
+    let classBadge: string;
+    switch (item) {
+      case 'deposit': {
+        classBadge = 'badge badge-success';
+        break;
+      }
+      case 'retire': {
+        classBadge = 'badge badge-danger';
+        break;
+      }
+      default: {
+        classBadge = 'badge badge-primary';
+        break;
+      }
+    }
+    return classBadge;
   }
 
 }
